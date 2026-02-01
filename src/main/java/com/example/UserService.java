1be56c14ff64e0a5065cc47b22a98d32a7aed373
HarithaRandunu <randunuharitha2001@gmail.com>
2026-02-01 19:41:34 +0530

    Fix Issues

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -10,29 +10,18 @@ public class UserService {
     private String password = "admin123";
 
     // VULNERABILITY: SQL Injection
-    public void findUser(String username) throws Exception {
+    public void findUser(String username) {
+        String query =
+            "SELECT * FROM users WHERE name = '" + username + "'";
 
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db",
-                "root", password);
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db",
+                    "root", password);
+             Statement st = conn.createStatement()) {
 
-        Statement st = conn.createStatement();
-
-        String query = "SELECT * FROM users WHERE name = '" + username + "'";
-
-        st.executeQuery(query);
-    }
-
-    // EVEN WORSE: another SQL injection
-    public void deleteUser(String username) throws Exception {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db",
-                "root", password);
-        Statement st = conn.createStatement();
-        String query = "DELETE FROM users WHERE name = '" + username + "'";
-        st.execute(query);
-    }
-
-    // SMELL: Unused method
-    public void notUsed() {
-        System.out.println("I am never called");
+            st.executeQuery(query);
+        } catch (java.sql.SQLException e) {
+            e.printStackTrace();
+            // Handle exception appropriately
+        }
     }
 }
